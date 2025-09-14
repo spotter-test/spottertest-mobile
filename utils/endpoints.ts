b861a531795bad6cf0bda48b8cb6f075a6de59ef
fynn emmanuel junior <fynn.emmanuel.junior@gmail.com>
2025-09-14 08:29:43 +0000

    updated splash screen logo

---
+++ utils/endpoints.ts
@@ -0,0 +1,7 @@
+const apiurl = 'http://192.168.0.100:5000/user'
+const flighturl = 'https://rapidapi.com/apiheya/api/sky-scrapper'
+
+
+//Auth
+export const signupUrl = `${apiurl}/signup`
+export const loginUrl = `${apiurl}/login`
